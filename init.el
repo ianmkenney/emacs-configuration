b60929c4fd01598a8af2cd3c43a414a49eb9fef6
Ian Kenney <ianmichaelkenney@gmail.com>
2024-11-06 18:35:52 -0700

    Corrected the name of the gruvbox package to gruvbox-theme

--- init.el
+++ init.el
@@ -16,16 +16,20 @@
 (use-package elfeed
   :ensure t
   :defer)
+
 (use-package company
   :config
   (global-company-mode t)
   :ensure t)
+
 (use-package ledger-mode
   :ensure t
   :defer)
+
 (use-package eglot
   :ensure t
   :defer)
+
 (use-package git-gutter
   :config
   (global-git-gutter-mode t)
@@ -33,16 +37,19 @@
   (global-set-key (kbd "M-g p") 'git-gutter:previous-hunk)
   (global-set-key (kbd "M-g n") 'git-gutter:next-hunk)
   :ensure t)
+
 (use-package magit
   :ensure t
   :defer)
+
 (use-package ivy
   :config
   (ivy-mode t)
   (setq ivy-use-virtual-buffers t
 	ivy-count-format "%d/%d")
   :ensure t)
-(use-package gruvbox
+
+(use-package gruvbox-theme
   :ensure t)
 
 (use-package htmlize
