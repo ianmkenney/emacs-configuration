37be27183484eb163fdc948d41d499cd76879ae5
Ian Kenney <ianmichaelkenney@gmail.com>
2025-07-24 16:53:04 -0400

    Improved pytest package to handle nested classes

--- lisp/pytest.el
+++ lisp/pytest.el
@@ -6,41 +6,55 @@
 
 (require 'project)
 
-(defvar pytest--function-name nil
-  "Variable to store the name of the Python function at point.")
+(defun pytest--get-enclosing-class (&optional depth)
+  (save-excursion
+    (end-of-line)
 
-(defvar pytest--class-name nil
-  "Variable to store the name of the Python class.")
+    (let ((sep "::")
+	  (indent nil)
+	  (name nil)
+	  (depth (or depth nil)))
 
-(defun pytest--extract-python-function-name ()
-  "Extract the Python function name under the point"
+    (when (re-search-backward "^\\([ \t]*\\)class \\([a-zA-Z_][a-zA-Z0-9_]*\\)" nil t)
+	(setq name (concat (substring-no-properties (match-string 2)))
+	      indent (length (substring-no-properties (match-string 1)))))
+
+    (if (not indent)
+	""
+      (progn
+	(if (equal indent 0) name
+	  (progn
+	    (previous-line)
+	    (when (not depth)
+	      (setq depth (+ 1 indent)))
+
+	    (if (< indent depth)
+		(concat (pytest--get-enclosing-class indent) sep name)
+	      (concat (pytest--get-enclosing-class indent) "")))))))))
+
+
+(defun pytest--get-qualified-name()
   (save-excursion
     (beginning-of-line)
-    ;; Set these both to nil by default.
-    (setq pytest--class-name nil)
-    (setq pytest--function-name nil)
-    ;; search for the function name
-    (when (re-search-forward "^[ \t]*def \\([a-zA-Z_][a-zA-Z0-9_]*\\)" (line-end-position) t)
-      (setq pytest--function-name (substring-no-properties (match-string 1)))
-      (save-excursion
-	(beginning-of-line)
-	(when (re-search-backward "^class \\([a-zA-Z_][a-zA-Z0-9_]*\\)" nil t)
-	  (setq pytest--class-name (substring-no-properties (match-string 1)))
-	  )
-      )
-    )))
+    (let ((function-name nil)
+	  (upper nil))
+      (when (re-search-forward "\\(^[ \t]*\\)def \\([a-zA-Z_][a-zA-Z0-9_]*\\)" (line-end-position) t)
+	(setq function-name (substring-no-properties (match-string 2))
+	      indent-depth (length (substring-no-properties (match-string 1)))))
+
+      (if (equal indent-depth 0)
+	  function-name
+	(concat (pytest--get-enclosing-class indent-depth) "::" function-name)))))
 
 (defun pytest--pyarg-target ()
   "Construct the pytest target test after extraction."
-  (let ((formatted (if pytest--class-name
-      (format "%s::%s::%s" buffer-file-name pytest--class-name pytest--function-name)
-      (format "%s::%s" buffer-file-name pytest--function-name))))
+  (let ((formatted
+	 (format "%s::%s" buffer-file-name (pytest--get-qualified-name))))
     (substring-no-properties formatted)))
 
 (defun pytest-compile-current-test ()
   "Extract the function and class under the point and provide it as a Makefile arg."
   (interactive)
-  (pytest--extract-python-function-name)
   (let ((default-directory (project-root (project-current t)))
         (compilation-buffer-name-function
          (or project-compilation-buffer-name-function
